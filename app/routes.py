2efa856354a3f5de32febb20bc7278a3e28884b2
VictorEmanuel08 <victorem3181@gmail.com>
2024-04-26 14:55:38 -0300

    Parâmetros por rota

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,10 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
-def index():
-    nome = "Victor"    
-    dados = {"profissao": "Desenvolvedor", "stack": "Python"}
+@app.route('/index', defaults={'nome':'usuário'})
+@app.route('/index/<nome>/<profissao>/<stack>')
+def index(nome, profissao, stack):
+    dados = {"profissao": profissao, "stack": stack}
     return render_template('index.html', nome=nome, dados=dados)
 
 @app.route('/contato')
